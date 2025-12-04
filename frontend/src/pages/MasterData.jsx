6c823ca156c839b4b4f1f67b8cc11113d6036f6e
Harish Lenkalapalli <dme.harishguptha@gmail.com>
2025-12-04 15:28:47 +0530

    second commit

--- frontend/src/pages/MasterData.jsx
+++ frontend/src/pages/MasterData.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import api from "../services/api";
-import "../styles/MasterData.css";
+import "../styles/Masterdata.css";
 
 /* ---------------------------------------------------
     IMAGE MATCHING – NO SLUG NEEDED
